ec311afa90bb7c69ac17c5f9de6addac2ee367c7
Henrietha <areathy33@gmail.com>
2021-07-29 19:17:01 +0100

    Playing around with components (selectors, templates and styling)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+// import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+// import { Server } from "http";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    // styleUrls: ['./Server.component.css']
+})
+export class ServerComponent {
+
+}
