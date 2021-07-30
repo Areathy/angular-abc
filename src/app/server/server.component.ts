8d9922d49c828e766ebcb763068b4e004b6cd437
Henrietha <areathy33@gmail.com>
2021-07-30 16:00:07 +0100

    Directives abc

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,15 +4,23 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    // styleUrls: ['./Server.component.css']
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 
     serverId: number = 2;
-    serverStatus: string = 'online';
+    serverStatus: string = 'offline';
 
-    getServerId() {
-        return this.serverId;
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 
 }
